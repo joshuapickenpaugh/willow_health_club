39f4c49bbf0c8e48c322f8835cf542ebd4ed7642
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-11 11:32:34 -0500

    Final Commit

--- Willow_Health_Club/Form1.Designer.vb
+++ Willow_Health_Club/Form1.Designer.vb
@@ -22,19 +22,178 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.rdoMale = New System.Windows.Forms.RadioButton()
+        Me.rdoFemale = New System.Windows.Forms.RadioButton()
+        Me.rdoActive = New System.Windows.Forms.RadioButton()
+        Me.rdoInactive = New System.Windows.Forms.RadioButton()
+        Me.txtWeight = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rdoFemale)
+        Me.GroupBox1.Controls.Add(Me.rdoMale)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(233, 54)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Gender:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.rdoInactive)
+        Me.GroupBox2.Controls.Add(Me.rdoActive)
+        Me.GroupBox2.Location = New System.Drawing.Point(10, 72)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(235, 55)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Activity Level:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 135)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Weight:"
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(15, 162)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(169, 42)
+        Me.btnCalc.TabIndex = 3
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(190, 162)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(55, 42)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(9, 215)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(236, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Total calories needed to maintain current weight:"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal.Location = New System.Drawing.Point(12, 234)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(233, 36)
+        Me.lblTotal.TabIndex = 6
+        Me.lblTotal.Text = " "
+        Me.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'rdoMale
+        '
+        Me.rdoMale.AutoSize = True
+        Me.rdoMale.Location = New System.Drawing.Point(35, 19)
+        Me.rdoMale.Name = "rdoMale"
+        Me.rdoMale.Size = New System.Drawing.Size(48, 17)
+        Me.rdoMale.TabIndex = 0
+        Me.rdoMale.TabStop = True
+        Me.rdoMale.Text = "Male"
+        Me.rdoMale.UseVisualStyleBackColor = True
+        '
+        'rdoFemale
+        '
+        Me.rdoFemale.AutoSize = True
+        Me.rdoFemale.Location = New System.Drawing.Point(137, 19)
+        Me.rdoFemale.Name = "rdoFemale"
+        Me.rdoFemale.Size = New System.Drawing.Size(59, 17)
+        Me.rdoFemale.TabIndex = 1
+        Me.rdoFemale.TabStop = True
+        Me.rdoFemale.Text = "Female"
+        Me.rdoFemale.UseVisualStyleBackColor = True
+        '
+        'rdoActive
+        '
+        Me.rdoActive.AutoSize = True
+        Me.rdoActive.Location = New System.Drawing.Point(37, 19)
+        Me.rdoActive.Name = "rdoActive"
+        Me.rdoActive.Size = New System.Drawing.Size(55, 17)
+        Me.rdoActive.TabIndex = 0
+        Me.rdoActive.TabStop = True
+        Me.rdoActive.Text = "Active"
+        Me.rdoActive.UseVisualStyleBackColor = True
+        '
+        'rdoInactive
+        '
+        Me.rdoInactive.AutoSize = True
+        Me.rdoInactive.Location = New System.Drawing.Point(139, 19)
+        Me.rdoInactive.Name = "rdoInactive"
+        Me.rdoInactive.Size = New System.Drawing.Size(63, 17)
+        Me.rdoInactive.TabIndex = 1
+        Me.rdoInactive.TabStop = True
+        Me.rdoInactive.Text = "Inactive"
+        Me.rdoInactive.UseVisualStyleBackColor = True
+        '
+        'txtWeight
+        '
+        Me.txtWeight.Location = New System.Drawing.Point(62, 132)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(181, 20)
+        Me.txtWeight.TabIndex = 7
         '
         'frmMain
         '
         Me.AllowDrop = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(255, 281)
+        Me.Controls.Add(Me.txtWeight)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "frmMain"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Willow Health Club"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents rdoFemale As RadioButton
+    Friend WithEvents rdoMale As RadioButton
+    Friend WithEvents rdoInactive As RadioButton
+    Friend WithEvents rdoActive As RadioButton
+    Friend WithEvents txtWeight As TextBox
 End Class
--- Willow_Health_Club/Form1.vb
+++ Willow_Health_Club/Form1.vb
@@ -1,3 +1,44 @@
-﻿Public Class frmMain
+﻿' Joshua Pickenpaugh
+' September 11th, 2016
+' "Willow Health Club" calculator. 
 
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Closes the app:
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+        'Declares variables:
+        Dim decClientWeight As Decimal
+        Dim decTotalCalories As Decimal
+
+        'Assigns user weight to variable:
+        Decimal.TryParse(txtWeight.Text, decClientWeight)
+
+        'Decision structure:
+        Select Case True
+            Case rdoMale.Checked
+                If rdoActive.Checked Then
+                    decTotalCalories = decClientWeight * 15
+                    lblTotal.Text = decTotalCalories
+                ElseIf rdoInactive.CheckAlign Then
+                    decTotalCalories = decClientWeight * 13
+                    lblTotal.Text = decTotalCalories
+                End If
+
+            Case rdoFemale.Checked
+                If rdoActive.Checked Then
+                    decTotalCalories = decClientWeight * 12
+                    lblTotal.Text = decTotalCalories
+                ElseIf rdoInactive.Checked Then
+                    decTotalCalories = decClientWeight * 10
+                    lblTotal.Text = decTotalCalories
+                End If
+        End Select
+
+    End Sub
 End Class
