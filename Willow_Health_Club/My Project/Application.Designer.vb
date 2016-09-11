811bd92f743cdb133eadf3756a954dd567aaf360
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-11 11:04:28 -0500

    First Commit

--- Willow_Health_Club/My Project/Application.Designer.vb
+++ Willow_Health_Club/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Willow_Health_Club.Form1
+            Me.MainForm = Global.Willow_Health_Club.frmMain
         End Sub
     End Class
 End Namespace
